b5449f2d2c9f17edb08f97836ac77e7dbe5cf72c
EmmanuelSHYIRAMBERE <emashyirambere1@gmail.com>
2023-11-01 09:51:34 +0200

    wewewe

--- controllers/Tours/makeTour.js
+++ controllers/Tours/makeTour.js
@@ -4,25 +4,25 @@ import cloudinary from "../../utility/cloudinary.js";
 import path from "path";
 
 export const addNewTour = catchAsyncError(async (req, res, next) => {
-  // const tourImagesArray = [];
-  // const backDropImage = await cloudinary.uploader.upload(
-  //   req.files["backDropImage"][0].path
-  // );
+  const tourImagesArray = [];
+  const backDropImage = await cloudinary.uploader.upload(
+    req.files["backDropImage"][0].path
+  );
 
-  // if (req.files["gallery"]) {
-  //   for (let index = 0; index < req.files["gallery"].length; index++) {
-  //     tourImagesArray.push(
-  //       await cloudinary.uploader.upload(req.files["gallery"][index].path)
-  //     );
-  //   }
-  // }
+  if (req.files["gallery"]) {
+    for (let index = 0; index < req.files["gallery"].length; index++) {
+      tourImagesArray.push(
+        await cloudinary.uploader.upload(req.files["gallery"][index].path)
+      );
+    }
+  }
 
   const newTour = await Tours.create({
     ...req.body,
-    backDropImage: "backDropImage.secure_url",
-    gallery: `req.files["gallery"]
+    backDropImage: backDropImage.secure_url,
+    gallery: req.files["gallery"]
       ? tourImagesArray.map((item) => item.secure_url)
-      : ""`,
+      : "",
   });
   return res.status(201).json({
     status: "Tour created successfully",
